7a3421e280dbabdc9fb3cb6be0180f5ab0572f14
zihao.chen <zihao.chen@xilancloud.com>
2025-11-13 18:19:23 +0800

    feat:init commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { useThemeStore } from '@/store'
+import { safeAreaInsets } from '@/utils/systemInfo'
+
+defineOptions({
+  name: 'Home',
+})
+definePage({
+  // 使用 type: "home" 属性设置首页，其他页面不需要设置，默认为page
+  type: 'home',
+  style: {
+    // 'custom' 表示开启自定义导航栏，默认 'default'
+    navigationStyle: 'custom',
+    navigationBarTitleText: '首页',
+  },
+})
+
+const themeStore = useThemeStore()
+
+const description = ref(
+  'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite5 + UnoCss + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
+)
+console.log('index/index 首页打印了')
+
+onLoad(() => {
+  console.log('测试 uni API 自动引入: onLoad')
+})
+</script>
+
+<template>
+  <view class="bg-white px-4 pt-2" :style="{ marginTop: `${safeAreaInsets?.top}px` }">
+   <wd-button>主要按钮123</wd-button>
+  </view>
+</template>
